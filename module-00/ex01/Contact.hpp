f570efe7ee3a2b97cddd14e3a79d0d55e2f3099e
M-Agoumi <agoumihunter@gmail.com>
2022-02-28 19:03:23 +0100

    add header to cpp/hpp files

--- module-00/ex01/Contact.hpp
+++ module-00/ex01/Contact.hpp
@@ -1,3 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magoumi <magoumi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 19:01:42 by magoumi           #+#    #+#             */
+/*   Updated: 2022/02/28 19:01:52 by magoumi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by magoumi on 28/2/2022.
+//
+
+#ifndef __CONTACT__
+#define __CONTACT__
 #include <iostream>
 
 class Contact
@@ -15,3 +33,5 @@ class Contact
 	private:
 		std::string truncate(std::string str);
 };
+
+#endif
