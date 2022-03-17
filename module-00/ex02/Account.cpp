04cf4ca0f101b3f43801722b1b56b765f1954434
M-Agoumi <agoumihunter@gmail.com>
2022-03-17 17:48:02 +0100

    ex02 init

---
+++ module-00/ex02/Account.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magoumi <magoumi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 19:14:06 by magoumi           #+#    #+#             */
+/*   Updated: 2022/03/05 19:27:05 by magoumi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int		Account::getNbAccounts( void )
+{
+	
+}
+
+int		Account::getTotalAmount( void )
+{
+	
+}
+
+int		Account::getNbDeposits( void )
+{
+	
+}
+
+int		Account::getNbWithdrawals( void )
+{
+	
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	
+}
+
+Account::Account( int initial_deposit )
+{
+	
+}
+
+Account::~Account( void )
+{
+	
+}
